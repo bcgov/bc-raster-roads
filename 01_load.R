c26bc17c9c3e4d8d5227d4766ad53df220ec8100
Don Morgan <don.morgan@gov.bc.ca>
2017-09-21 14:10:49 -0700

    Initial script

--- 01_load.R
+++ 01_load.R
@@ -9,5 +9,43 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
+require(rgdal)
+
+#Set enviornment variables and create directories
+RepoName<-'bc_road_analysis'
+
+#fileDir<-paste('/Users/Morgan/Dropbox/_dev/',RepoName,'/',sep='')
+fileDir<-paste('/Users/donmorgan/Dropbox (BVRC)/_dev/',RepoName,'/',sep='')
+OutDir<-paste(fileDir,'out/',sep='')
+figsOutDir<-paste(OutDir,'figures/',sep='')
+dataOutDir<-paste(OutDir,'data/',sep='')
+dir.create(file.path(OutDir), showWarnings = FALSE)
+dir.create(file.path(figsOutDir), showWarnings = FALSE)
+dir.create(file.path(dataOutDir), showWarnings = FALSE)
+DataDir <- paste(fileDir,"data/",sep='')
+setwd(OutDir)
+
+
+#Consolidated roads will be downloadable from some CE maintained location data
+ConRdsFile<-'BC_CE_IntegratedRoads_2017_v1_20170114_no_TRIM'
+ConRds_zip <- paste(DataDir,ConRdsFile,".zip",sep='')
+
+unzip(ConRds_zip, exdir = DataDir, overwrite = TRUE)
+
+#zipped and zip files may not have the same name - search directory for gdb
+Rd_gdb<-paste(DataDir,(list.files(path=paste(DataDir,sep=''), pattern='gdb')[1]),sep='')
+
+# List feature classes in the geodatabase
+fc_list <- ogrListLayers(Rd_gdb)
+print(fc_list)
+
+# Read integrated roads feature class-usually the first record in list
+IntRds <- readOGR(dsn=Rd_gdb,layer=fc_list[1])
+
+#set projection to BC Albers
+crs(IntRds) <- "+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=45 +lon_0=-126 +x_0=1000000 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"
+
+# Determine the FC extent, projection, and attribute information
+summary(IntRds)
 
 
--- 02_clean.R
+++ 02_clean.R
@@ -10,4 +10,53 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+require(sp)
+require(raster)
+require(rgdal)
+require(spatstat)
+require(maptools)
+dir.create("tmp", showWarnings = FALSE)
 
+#Calculate time for routine to run
+ptm <- proc.time()
+roadsIN<-IntRds
+#Set up Provincial raster based on hectares BC extent, 1ha resolution and projection
+ProvRast<-raster(nrows=15744, ncols=17216, xmn=159587.5, xmx=1881187.5, ymn=173787.5,ymx=1748187.5,crs="+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=45 +lon_0=-126 +x_0=1000000 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
+
+# Convert to a line segment pattern object using maptools
+roadsPSP <- as.psp(as(roadsIN, 'SpatialLines')) #approx 3 mins with Morice
+proc.time() - ptm
+
+#Calculate time for routine to run
+ptm <- proc.time()
+
+#use map extents to set dimensions for the pixellate used to calculate lengths per cell
+#e <- extent( roadsIN )#used with the smaller test data
+e <- extent( ProvRast )
+RdExt<-c((e[4]-e[3]-1),(e[2]-e[1]-1))/100 #should result in 1 ha 100x100 cells based on ProvRast
+roadLengthIM <- pixellate.psp(roadsIN, dimyx=RdExt)
+
+# Convert pixel image to a raster with km roads in each cell
+roadlength<-raster(roadLengthIM/1000)
+prod(roadlength)
+#check output if resolution is not exactly 100 then need to re set resolution
+#Set raster to have 1ha cells so maps onto Provincial raster (hectares BC format)
+#roadlengthP<-resample(rdLeng,ProvRast,method='bilinear')
+proc.time() - ptm #1,802,111 cells took 122 seconds, 271,048,704 for the province ?
+
+#write out road length raster as a geotif
+rf <- writeRaster(roadlength, filename="rdLength.tif", format="GTiff", overwrite=TRUE)
+#rdLeng<-raster("rdLength.tif")
+
+#Some code snipets if need to break Province into tiles for processing
+library(SpaDES) #for splitting up large rasters
+
+#split raster into smaller chunks for processing
+rTiles=splitRaster(ProvRaster, nx=10, ny=10, path=DataDir)#100 tiles
+
+#Loop through each tile and calclate lenght of road in each cell
+i<-1
+rs<-rTiles[[i]]
+
+rs[] <- 1:ncell(rs)
+.....
