6bdfdb2de9e94441efd17bd1fa8797bb971c93c2
Andy Teucher <andy.teucher@gov.bc.ca>
2018-02-20 11:05:40 -0800

    use file.path; remove figsOutDir as not used

--- header.R
+++ header.R
@@ -2,13 +2,11 @@ library(sf)
 library(dplyr)
 library(readr)
 
-OutDir<-('out/')
-figsOutDir<-paste(OutDir,'figures/',sep='')
-dataOutDir<-paste(OutDir,'data/',sep='')
-tileOutDir<-paste(dataOutDir,'tile/',sep='')
-DataDir <- "data"
+OutDir <- 'out'
+dataOutDir <- file.path(OutDir,'data')
+tileOutDir <- file.path(dataOutDir,'tile')
+DataDir <- 'data'
 dir.create(file.path(OutDir), showWarnings = FALSE)
-dir.create(file.path(figsOutDir), showWarnings = FALSE)
 dir.create(file.path(dataOutDir), showWarnings = FALSE)
 dir.create(file.path(tileOutDir), showWarnings = FALSE)
 dir.create(DataDir, showWarnings = FALSE)
