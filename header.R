e6d6914fdfd195737bddb4f835fc6f3215fc5e8b
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2018-02-22 15:20:28 -0800

    add tmp

--- header.R
+++ header.R
@@ -2,11 +2,13 @@ library(sf)
 library(dplyr)
 library(readr)
 
+TmpDir <- 'tmp'
 OutDir <- 'out'
+DataDir <- 'data'
 dataOutDir <- file.path(OutDir,'data')
 tileOutDir <- file.path(dataOutDir,'tile')
-DataDir <- 'data'
 dir.create(file.path(OutDir), showWarnings = FALSE)
 dir.create(file.path(dataOutDir), showWarnings = FALSE)
 dir.create(file.path(tileOutDir), showWarnings = FALSE)
 dir.create(DataDir, showWarnings = FALSE)
+dir.create(TmpDir, showWarnings = FALSE)
